323e552b57c027bcf78b19e0bc23efcd796d5cab
Onesio Junior <onesiojr1@gmail.com>
2024-08-06 16:15:55 -0300

    feat: add movies comming from api to show on screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:ilia_flutter_challenge/model/movie.dart';
-import 'package:ilia_flutter_challenge/widgets/movie_item.dart';
+import 'package:ilia_flutter_challenge/widgets/organisms/movie_list.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final _scrollController = ScrollController();
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: const Color(0xFF07041f),
       body: SingleChildScrollView(
+        controller: _scrollController,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),//TODO: verificar como remover padding vertical e usar algum outro valor mais correto
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
           child: Column(
             children: [
               const Text(
@@ -22,16 +24,8 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 32),
-              MovieItem(
-                movie: Movie(
-                  genreIds: [1,2,3], 
-                  id: 533535, 
-                  overview: "Um apático Wade Wilson trabalha duro na vida civil. Seus dias como mercenário moralmente flexível, Deadpool, ficou para trás. Quando seu planeta enfrenta uma ameaça, Wade deve relutantemente vestir-se novamente com um ainda mais relutante... relutante? Mais relutante? Ele deve convencer um Wolverine relutante em... Porra. As sinopses são tão estúpidas.", 
-                  popularity: 23022.328, 
-                  posterPath: "/qkhZRqCWqJ376sBzD4MeAO2w4wv.jpg", 
-                  releaseDate: "2024-07-24",
-                  title: "Deadpool & Wolverine",
-                ),
+              MovieList(
+                scrollController: _scrollController,
               ),
             ],
           ),
